ad36249a328767e2700707e94259a00d6ec27fff
hashtagrik <3207135+hashtagrik@users.noreply.github.com>
2024-12-23 15:29:21 +0100

    Start over

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://hashtagrik.github.io', 
-    base: 'hashtagrik.github.io',
-});
