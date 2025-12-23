4ea84244a4af396e9604aba7e94eae8e9c640ab0
hashtagrik <3207135+hashtagrik@users.noreply.github.com>
2025-12-23 16:57:01 +0100

    Added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hashtagrik.github.io',
+  site: 'https://hashtagrik.github.io',
+  integrations: [mdx()],
 });
